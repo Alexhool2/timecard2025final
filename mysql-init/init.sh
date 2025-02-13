04c4c91f40793abb2193ca9808325d431c50409c
Alexhool2 <alederbal@hotmail.com>
2025-02-14 05:34:15 +0900

    sql config

--- mysql-init/init.sh
+++ mysql-init/init.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-envsubst < /docker-entrypoint-initdb.d/init.sql.template > /docker-entrypoint-initdb.d/init.sql
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" < /docker-entrypoint-initdb.d/init.sql
+set -e
+
+# Se o diretório de dados estiver vazio (primeira inicialização), substitui o template
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Substituindo variáveis no init.sql.template..."
+    envsubst < /docker-entrypoint-initdb.d/init.sql.template > /docker-entrypoint-initdb.d/init.sql
+fi
+
+# Chama o entrypoint original do MySQL (cujo caminho é /usr/local/bin/docker-entrypoint.sh)
+exec /usr/local/bin/docker-entrypoint.sh "$@"
